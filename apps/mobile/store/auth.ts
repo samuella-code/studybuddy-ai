7698e8fda9881e71efc555cd7e6b768c7377ec98
Eze - Oge Samuella <ezeogesamuella@gmail.com>
2026-09-07 13:15:42 +0100

    Clear all local learner data on logout

--- apps/mobile/store/auth.ts
+++ apps/mobile/store/auth.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 import type { User } from "../services/api";
 import { clearStoredSession, loadSession, saveSession } from "../services/auth-storage";
+import { clearSubjects } from "../services/subjects-storage";
 
 type AuthState = {
   user: User | null;
@@ -21,11 +22,17 @@ export const useAuthStore = create<AuthState>((set) => ({
     void saveSession(accessToken, user);
   },
   hydrate: async () => {
-    const session = await loadSession();
-    set(session ? { user: session.user, accessToken: session.token, hydrated: true } : { hydrated: true });
+    try {
+      const session = await loadSession();
+      set(session
+        ? { user: session.user, accessToken: session.token, hydrated: true }
+        : { hydrated: true });
+    } catch {
+      set({ user: null, accessToken: null, hydrated: true });
+    }
   },
   clearSession: async () => {
-    await clearStoredSession();
-    set({ user: null, accessToken: null });
+    await Promise.all([clearStoredSession(), clearSubjects()]);
+    set({ user: null, accessToken: null, hydrated: true });
   },
 }));
